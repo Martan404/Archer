0da0c7d2c464af4eec629ad6d82f1f0cfb37d8d7
Martan404 <maartaan@gmail.com>
2024-08-13 18:38:26 +0200

    Add files via upload

--- archer.sh
+++ archer.sh
@@ -112,14 +112,20 @@ fi
 }
 
 set_keymap() {
-	echo -e "-------------------------------------------------------------------------"
-	echo -e "Listing available keyboard keymaps"
+	local status=1
+	while [ $status -ne 0 ]; do
+		echo -e "-------------------------------------------------------------------------"
+		echo -e "Listing available keyboard keymaps"
 
-	localectl list-keymaps | awk '{printf "%s  ", $0} END {print ""}'
+		localectl list-keymaps | awk '{printf "%s  ", $0} END {print ""}'
 
-	echo -e "-------------------------------------------------------------------------"
-	echo -e "Enter keymap to use for console and X11"
-	read -r -p "Name: " keyboard_keymap
+		echo -e "-------------------------------------------------------------------------"
+		echo -e "Enter keymap to use for system"
+		read -r -p "Name: " keyboard_keymap
+
+		localectl set-keymap "$keyboard_keymap"
+		status=$?
+	done
 
 	while true; do
 		read -r -p "Is $keyboard_keymap correct? (Y/n) " yesNo
